ad8fcf48c7f393fe12ddfc41a97f30dc717a12fc
omiras <oscar.miras@gmail.com>
2022-07-07 17:57:10 +0200

    insertOne

--- app.js
+++ app.js
@@ -14,11 +14,7 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true,
 });
 
-// Usamos el método connect del objecto client para conectarnos a la BBDD
-client.connect(async () => {
-    // Esta función se ejcuta un vez te has conectado a la base de datos
-    console.log('Conectado a la base de datos correctamente.');
-
+async function findRestaurants() {
     // Seleccionar la base de datos
     const database = client.db('sample_restaurants');
 
@@ -30,7 +26,7 @@ client.connect(async () => {
         borough: "Bronx"
     });
 
-    //console.log(document)
+    console.log(document)
 
     // Obtener varios documentos
     // Todos los restaurantes que son del Bronx y sirven comida American
@@ -39,6 +35,41 @@ client.connect(async () => {
         cuisine: "American"
     }).toArray();
     console.log(americanRestaurants.length);
+
+}
+
+async function insertRestaurant() {
+    // Seleccionar la base de datos
+    const database = client.db('sample_restaurants');
+
+    // Seleccionamos la colección restaurants
+    const restaurants = database.collection('restaurants');
+
+    // Insertar nuevo restaurante
+    const document = await restaurants.insertOne({
+        "address": {
+            "building": "707",
+            "coord": [
+                -73.9653967,
+                40.6064339
+            ],
+            "street": "Kings Highway",
+            "zipcode": "11223"
+        },
+        "borough": "Brooklyn",
+        "cuisine": "Jewish/Kosher",
+        "name": "The Happy Jew",
+        "restaurant_id": "401234567"
+    });
+
+    console.log(document);
+}
+
+// Usamos el método connect del objecto client para conectarnos a la BBDD
+client.connect(async () => {
+    // Esta función se ejcuta un vez te has conectado a la base de datos
+    console.log('Conectado a la base de datos correctamente.');
+    await insertRestaurant();
 
     // cerrar la conexión
     await client.close();
